c2723beaa7db9176e592ecb5cb9265d1ab46282f
AbdulBeard <abdulkhaliqzaheer@gmail.com>
2018-10-31 16:15:28 -0400

    adding in missing project files

---
+++ funcy/Class1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
+using System;
+using System.Threading.Tasks;
+
+namespace funcy
+{
+    public class Class1
+    {
+        public static async Task<double> Yolo()
+        {
+            var result = await CSharpScript.EvaluateAsync<double>("Sqrt(2 + 2)", ScriptOptions.Default.WithImports("System.Math"));
+            return result;
+        }
+    }
+}
